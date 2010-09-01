5f39128a82354a233f534902afe43751d4ae7f0c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-09-01 13:16:57 +0000

    implement root finding

--- foifm.R
+++ foifm.R
@@ -1,6 +1,6 @@
 foifm <- function(mixing, b, gamma, mu)
   {
-    beta <- matrix(nrow == nrow(mixing), ncol == nrow(mixing))
+    beta <- matrix(NA, nrow(mixing), ncol(mixing))
     for (i in 1:nrow(mixing)) {
       for (j in 1:nrow(mixing)) {
         beta[i,j]=b[mixing[i,j]]
@@ -12,5 +12,9 @@ foifm <- function(mixing, b, gamma, mu)
       as.vector(r)
     }
 
-    
+    l0 <- rep(1,nrow(mixing))
+
+    ans <- dfsane(par = l0, fn = foi, control = list(trace = FALSE))
+
+    ans$par
   }
