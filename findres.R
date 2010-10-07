37b6b5a498556070212392c167ddb9576b5f6815
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-10-07 17:43:36 +0000

    give info

--- findres.R
+++ findres.R
@@ -3,13 +3,20 @@ findres <- function(NGM, projection, depth = 1, start = 1)
     res <- FALSE
     if (depth == 1) {
       for (i in c(start:ncol(NGM))) {
-        unit=diag(1,38,38)
+        unit=diag(1,nrow(NGM),ncol(NGM))
         proj <- projection
         proj[i,i] = 1
         U <- proj %*% NGM
         Q <- (unit-proj) %*% NGM
         u_sr <- max(abs(eigen(U)$values))
         q_sr <- max(abs(eigen(Q)$values))
+        pvec <- c()
+        for (j in 1:nrow(proj))  {
+          if (proj[j,j] == 1) {
+            pvec <- c(pvec, j)
+          }
+        }
+        cat ("pvec = ", pvec, ", usr = ", u_sr, ", q_sr = ", q_sr, "\n")
         if (u_sr > 1 && q_sr < 1) {
           species <- c()
           for (i in c(1:ncol(NGM))) {
