37ea82fdc10bde16b7c8cbd489aaa1bb423bb24b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-10-12 16:37:13 +0000

    remove comments, add gambiense and nonzero options

--- reservoirs_old.R
+++ reservoirs_old.R
@@ -20,7 +20,10 @@ opt = getopt(c(
   'mixing', 'm', 1, "character",
   'data', 'd', 1, "character",
   'density', 'y', 0, "logical",
-  'iterations', 'i', 1, "integer"
+  'iterations', 'i', 1, "integer",
+  'ignorezeroes', 'z', 0, "logical",
+  'gambiense', 'g', 0, "logical",
+  'nongambiense', 'n', 0, "logical"
 ));
 
 # read data
@@ -31,22 +34,46 @@ data <- read.csv(file=opt$data, head=TRUE, sep=",")
 # structure of the mixing matrix from csv file
 stopifnot ( !is.null(opt$mixing) );
 
-M <- data$pos_tbg + data$pos_tbng
+if (is.null(opt$gambiense)) {
+   if (is.null(opt$nongambiense)) {
+     M <- data$pos_tbg + data$pos_tbng
+   } else {
+     M <- data$pos_tbng
+   }
+} else {
+  M <- data$pos_tbg
+}
+
+
 N <- data$N
 mu <- data$mortality
 gamma <- data$rec_rate
 
-ndata <- nrow(data)
-
-cat ("Target prev=", M/N, "\n")
+if (!is.null(opt$ignorezeroes)) {
+  N <- N[M>0]
+  mu <- mu[M>0]
+  gamma <- gamma[M>0]
+}
 
 if (opt$mixing == "random") {
   mixing_matrix <- NA
   b <- rep(1, length(N))
 } else {
-  mixing_matrix <- read.csv(file=opt$mixing, head=FALSE, sep=",")
-  b <- rep(1, max(mixing_matrix))
+  mixing_matrix <- as.matrix(read.csv(file=opt$mixing, head=FALSE, sep=","))
+  if (!is.null(opt$ignorezeroes)) {
+    mixing_matrix <- mixing_matrix[M>0,][,M>0]
+  }
+  mixing_els <- unique(as.vector(mixing_matrix))
+  b <- rep(0, max(mixing_matrix))
+  b[mixing_els] <- 1
 }
+
+if (!is.null(opt$ignorezeroes)) {
+  M <- M[M>0]
+}
+
+cat ("Target prev=", M/N, "\n")
+cat ("Species: ", rownames(data)[M>0], "\n")
 
 if (is.null(opt$iterations)) {
   iter <- 1000
@@ -63,33 +90,17 @@ for (i in 1:iter) {
   
   mult <- FALSE
 
-#  nr <- sample(length(b), 1)
-#  nr <- 1
   nr <- ceiling(abs(rmnorm(1, 0, 2)))
 
   el <- sample(length(b), nr)
-#  b[el] <- rmnorm(1, b[el], diag(b[el]/100, nrow=nr, ncol=nr))
-#  b[b<0] <- 0 
-#  r <- sample(9*4, nr, replace = TRUE)
   for (j in 1:nr) {
-#    if (r[j] > 18) {
-#      r[j] <- r[j] - 18 
-#      if (r[j] > 9) {
-#        r[j] <- r[j] - 9
-#        b[el[j]] <- b[el[j]] * r[j]
-#      } else {
-#        b[el[j]] <- b[el[j]] / r[j]
-#      }
-#    } else {
-      nonzero <- TRUE
-      while (nonzero == TRUE) {
-        b[el[j]] <- rnorm(1, mean=b[el[j]], sd=b[el[j]]/10)
-#        cat(j, " ", b[el[j]], "\n")
-        if (b[el[j]]>0) { nonzero <- FALSE }
-      }
+    nonzero <- TRUE
+    while (nonzero == TRUE) {
+      b[el[j]] <- rnorm(1, mean=b[el[j]], sd=b[el[j]]/10)
+      if (b[el[j]]>0) { nonzero <- FALSE }
     }
-#  }
-      
+  }
+  
   prev <- pfm(pars=b, gamma=gamma, mu=mu, mixing_structure=mixing_matrix, 
               density=!is.null(opt$density), N=N)
   cat ("i=", i, ", testb=", b, "\n")
@@ -97,9 +108,6 @@ for (i in 1:iter) {
   l <- ilikelihood(prev, mu, gamma, M, N)
 
   cat ("i=", i, ", savel=", savel, ", l=", l, ", savel-l=", savel-l, "\n")
-#  accept <- min(c(1, exp(-(savel-l))))
-#  cat ("i=", i, ", accept=",accept,"savel=",savel,"l=",l,"\n")
-#  if (!is.nan(accept) && runif(1) < accept) {
    if (!is.nan(l) && l > savel) {
     # accept
     cat ("i=", i, ", accepted\n")
