455c95e01383caa29ada68f46a7fae993fc9b0ee
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-10-12 22:53:55 +0000

    plot up to max data

--- plot_prevs.R
+++ plot_prevs.R
@@ -6,13 +6,13 @@ plot_prevs <- function(pars, gamma, mu, M, mixing_structure = NA,
   source('pfm.R')
   source('mixing.R')
   
-  prev <- pfm(b, gamma=gamma, mu=mu, mixing_structure=mixing_structure,
+  prev <- pfm(pars=pars, gamma=gamma, mu=mu, mixing_structure=mixing_structure,
            density=density, N=N)*N
-  maxdata <- max(c(prev, M))
+  maxdata <- max(c(prev, M))*1.1
   par(mar = c(12, 3, 1, 1), las=2)
-  plot(prev, col="red", pch=19, xlab="", ylab="# infected",axes=FALSE, ylim=c(0,maxdata*1.1))
+  plot(prev, col="red", pch=19, xlab="", ylab="# infected",axes=FALSE, ylim=c(0,maxdata))
   par(new=T)
-  plot(M, col="blue", axes=F, pch="+", ylab="", xlab="")
+  points(M, col="blue", axes=F, pch="+", ylab="", xlab="")
   axis(1,labels=labels,at=1:38,lwd.ticks=0)
-  axis(2,at=20*0:120)
+  axis(2,at=20*(0:maxdata))
 }
