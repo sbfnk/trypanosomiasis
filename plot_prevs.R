14c5c01ae95f7c731bb4aad2ae5e7175c40cb75d
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-10-13 21:02:23 +0000

    better rotating of axis labels

--- plot_prevs.R
+++ plot_prevs.R
@@ -9,10 +9,10 @@ plot_prevs <- function(pars, gamma, mu, M, mixing_structure = NA,
   prev <- pfm(pars=pars, gamma=gamma, mu=mu, mixing_structure=mixing_structure,
            density=density, N=N)*N
   maxdata <- max(c(prev, M))*1.1
-  par(mar = c(12, 3, 1, 1), las=2)
+  par(mar = c(12, 3, 1, 1))
   plot(prev, col="red", pch=19, xlab="", ylab="# infected",axes=FALSE, ylim=c(0,maxdata))
   par(new=T)
-  points(M, col="blue", axes=F, pch="+", ylab="", xlab="")
-  axis(1,labels=labels,at=1:38,lwd.ticks=0)
-  axis(2,at=20*(0:maxdata))
+  points(M, col="blue", pch="+", ylab="", xlab="")
+  axis(1,labels=labels,at=1:length(N),lwd.ticks=0, las=2)
+  axis(2,at=20*(0:maxdata), las=3)
 }
