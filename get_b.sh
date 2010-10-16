fa11367372bb4728799ec5e98c99cc9e51e3467a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2010-10-16 11:17:20 +0000

    make more generic

--- get_b.sh
+++ get_b.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 pref=""
-if [ -n $2 ];
+if [ -n "$2" ];
 then
-    pref="$2 -> "
+    pref="$2 <- "
 fi
     
 echo $pref""c\($(tail -n 1 $1 | sed 's/^.* b= //g' | sed 's/ /,/g' | sed 's/[ ,]*$//g')\)
