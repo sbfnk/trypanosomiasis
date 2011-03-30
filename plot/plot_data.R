bb30dc38b435c5a8eddbdb4788f74027ce82334c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-03-30 19:53:52 +0000

    update

--- plot/plot_data.R
+++ plot/plot_data.R
@@ -8,42 +8,15 @@ library('getopt');
 library('ggplot2');
 
 opt = getopt(c(
-  'file', 'f', 1, "character",
-  'data', 'd', 1, "character",
-  'vector', 'v', 1, "character",
-  'params', 'p', 1, "character",
-  'area_convert', 'a', 0, "logical",
-  'alpha', 'l', 0, "logical"
+  'file', 'f', 1, "character"
 ))
 
-# read data
-stopifnot ( !is.null(opt$data) );
-stopifnot ( !is.null(opt$vector) );
-data <- read.csv(file=opt$data, head=TRUE, sep=",")
-vector <- read.csv(file=opt$vector, head=TRUE, sep=",")
-params <- read.csv(file=opt$params, head=TRUE, sep=",")
+offset <- 14
 
-offset <- 13
-if (!is.null(opt$alpha)) {
-  offset <- offset + 1
-}
-
-rmu <- data$mortality
-vmu <- vector$mortality
-rgamma <- data$rec_rate
-theta <- data$theta
-rabundance <- data$abundance
-vdensity <- vector$density
-biting_rate <- vector$biting_rate
-
-if (!is.null(opt$area_convert)) {
-  area_convert <- params$area_convert
-} else {
-  area_convert <- 1
-}
-
-
-res_data <- matrix(scan(opt$file), byrow=TRUE, ncol=2*offset-1)
+tab <- read.table(opt$file, header=T, sep=",")
+nam <- names(tab)
+res_data <- as.matrix(tab) 
+#res_data <- matrix(scan(opt$file), byrow=TRUE, ncol=2*(offset-1))
 
 pdf(paste("human_data.pdf"))
 human_data <- sqrt(res_data[,offset+1])
@@ -52,23 +25,23 @@ lines(density(human_data), col="red")
 CIrand <- quantile(x=human_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(human_data>1)/1000000))
+mtext(paste("p(R0>1) =", sum(human_data>1)/nrow(res_data)))
 dev.off()
 
 pdf(paste("species_data.pdf"))
 for (i in 1:12)  {
   species_data<- sqrt(res_data[,i+offset])
-  hist(main=data$name[i], breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
+  hist(main=nam[i+offset], breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
   lines(density(species_data), col="red")
   CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
   abline(v=CIrand, col="blue", lwd=2)
   abline(v=1, col="green", lwd=2)
-  mtext(paste("p(R0>1) =", sum(species_data>1)/1000000))
+  mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 }
 dev.off()
 
 pdf(paste("domestic.pdf"))
-species_data <- rep(0, 1000000)
+species_data <- rep(0, nrow(res_data))
 for (i in 2:4) {
   species_data <- species_data +
     res_data[,i+offset]
@@ -79,11 +52,11 @@ lines(density(species_data), col="red")
 CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/1000000))
+mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 dev.off()
 
 pdf(paste("wildlife.pdf"))
-species_data <- rep(0, 1000000)
+species_data <- rep(0, nrow(res_data))
 for (i in 5:12) {
   species_data <- species_data +
     res_data[,i+offset]
@@ -94,11 +67,11 @@ lines(density(species_data), col="red")
 CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/1000000))
+mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 dev.off()
 
 pdf(paste("dom_wild.pdf"))
-species_data <- rep(0, 1000000)
+species_data <- rep(0, nrow(res_data))
 for (i in 2:12) {
   species_data <- species_data +
     res_data[,i+offset]
@@ -109,7 +82,7 @@ lines(density(species_data), col="red")
 CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/1000000))
+mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 dev.off()
 
 ## pdf(paste("human_vs_wildlife.pdf"))
@@ -118,7 +91,7 @@ dev.off()
 ## dev.off()
 
 pdf(paste("r0.pdf"))
-species_data <- rep(0, 1000000)
+species_data <- rep(0, nrow(res_data))
 for (i in 1:12) {
   species_data <- species_data +
     res_data[,i+offset]
@@ -129,7 +102,7 @@ lines(density(species_data), col="red")
 CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/1000000))
+mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 dev.off()
 
 if (!is.null(opt$alpha)) {
