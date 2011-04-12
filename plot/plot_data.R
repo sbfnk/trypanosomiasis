7c9a925ad64bc88b2f7551d9623c350837c10e4c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-04-12 11:24:19 +0000

    more options

--- plot/plot_data.R
+++ plot/plot_data.R
@@ -8,69 +8,90 @@ library('getopt');
 library('ggplot2');
 
 opt = getopt(c(
-  'file', 'f', 1, "character"
+  'file', 'f', 1, "character",
+  'pdfname', 'p', 1, "character",
+  'offset', 'o', 1, "integer",
+  'alpha', 'a', 0, "logical",
+  'noplot', 'n', 0, "logical"
 ))
 
-offset <- 14
+if (is.null(opt$offset)) {
+  offset <- 14
+} else {
+  offset <- opt$offset
+}
+
+if (is.null(opt$pdfname)) {
+  pdfname <- "reservoirs.pdf"
+} else {
+  pdfname <- opt$pdfname
+}
 
 tab <- read.table(opt$file, header=T, sep=",")
 nam <- names(tab)
 res_data <- as.matrix(tab) 
 #res_data <- matrix(scan(opt$file), byrow=TRUE, ncol=2*(offset-1))
 
-pdf(paste("human_data.pdf"))
-human_data <- sqrt(res_data[,offset+1])
-hist(main="Human", breaks=100, freq=F, x=human_data, xlab="R0", ylab="Likelihood")
-lines(density(human_data), col="red")
-CIrand <- quantile(x=human_data, probs=c(0.025, 0.975))
-abline(v=CIrand, col="blue", lwd=2)
-abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(human_data>1)/nrow(res_data)))
-dev.off()
+if (is.null(opt$noplot)) {
+  pdf(paste(pdfname))
+}
+## human_data <- sqrt(res_data[,offset+1])
+## hist(main="Human", breaks=100, freq=F, x=human_data, xlab="R0", ylab="Likelihood")
+## lines(density(human_data), col="red")
+## CIrand <- quantile(x=human_data, probs=c(0.025, 0.975))
+## abline(v=CIrand, col="blue", lwd=2)
+## abline(v=1, col="green", lwd=2)
+## mtext(paste("p(R0>1) =", sum(human_data>1)/nrow(res_data)))
+## dev.off()
 
-pdf(paste("species_data.pdf"))
+## pdf(paste("species_data.pdf"))
 for (i in 1:12)  {
   species_data<- sqrt(res_data[,i+offset])
-  hist(main=nam[i+offset], breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
+  h <- hist(main=nam[i+offset], breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
+  lines(density(species_data), col="red")
+  CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
+  abline(v=CIrand, col="blue", lwd=2)
+  abline(v=1, col="green", lwd=2)
+  mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
+  if (i == 1) {
+    cat("humans",h$mids[h$intensities==max(h$intensities)],CIrand,"\n");
+  }
+}
+## dev.off()
+
+## pdf(paste("domestic.pdf"))
+if (is.null(opt$noplot)) {
+  species_data <- rep(0, nrow(res_data))
+  for (i in 2:4) {
+    species_data <- species_data +
+      res_data[,i+offset]
+  }
+  species_data <- sqrt(species_data)
+  hist(main="Domestic cycle", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
+  lines(density(species_data), col="red")
+  CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
+  abline(v=CIrand, col="blue", lwd=2)
+  abline(v=1, col="green", lwd=2)
+  mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
+  ## dev.off()
+
+  ## pdf(paste("wildlife.pdf"))
+  species_data <- rep(0, nrow(res_data))
+  for (i in 5:12) {
+    species_data <- species_data +
+      res_data[,i+offset]
+  }
+  species_data <- sqrt(species_data)
+  hist(main="Wildlife cycle", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
   lines(density(species_data), col="red")
   CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
   abline(v=CIrand, col="blue", lwd=2)
   abline(v=1, col="green", lwd=2)
   mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
 }
-dev.off()
+## dev.off()
 
-pdf(paste("domestic.pdf"))
-species_data <- rep(0, nrow(res_data))
-for (i in 2:4) {
-  species_data <- species_data +
-    res_data[,i+offset]
-}
-species_data <- sqrt(species_data)
-hist(main="Domestic cycle", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
-lines(density(species_data), col="red")
-CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
-abline(v=CIrand, col="blue", lwd=2)
-abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
-dev.off()
-
-pdf(paste("wildlife.pdf"))
-species_data <- rep(0, nrow(res_data))
-for (i in 5:12) {
-  species_data <- species_data +
-    res_data[,i+offset]
-}
-species_data <- sqrt(species_data)
-hist(main="Wildlife cycle", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
-lines(density(species_data), col="red")
-CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
-abline(v=CIrand, col="blue", lwd=2)
-abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
-dev.off()
-
-pdf(paste("dom_wild.pdf"))
+## pdf(paste("dom_wild.pdf"))
 species_data <- rep(0, nrow(res_data))
 for (i in 2:12) {
   species_data <- species_data +
@@ -83,38 +104,41 @@ CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
 abline(v=CIrand, col="blue", lwd=2)
 abline(v=1, col="green", lwd=2)
 mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
-dev.off()
+cat("animals",h$mids[h$intensities==max(h$intensities)],CIrand,"\n");
+## dev.off()
 
 ## pdf(paste("human_vs_wildlife.pdf"))
 ## df=data.frame(humans=human_data, animals=species_data)
 ## qplot(data=df, humans, animals, geom="point")
 ## dev.off()
 
-pdf(paste("r0.pdf"))
-species_data <- rep(0, nrow(res_data))
-for (i in 1:12) {
-  species_data <- species_data +
-    res_data[,i+offset]
-}
-species_data <- sqrt(species_data)
-hist(main="R0", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
-lines(density(species_data), col="red")
-CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
-abline(v=CIrand, col="blue", lwd=2)
-abline(v=1, col="green", lwd=2)
-mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
-dev.off()
-
-if (!is.null(opt$alpha)) {
-  pdf(paste("vprev.pdf"))
-  hist(main="vprev", breaks=100, freq=F, x=res_data[,offset], xlab="vprev", ylab="Likelihood")
-  lines(density(res_data[,offset]), col="red")
-  CIrand <- quantile(x=res_data[,offset], probs=c(0.025, 0.975))
+## pdf(paste("r0.pdf"))
+if (is.null(opt$noplot)) {
+  species_data <- rep(0, nrow(res_data))
+  for (i in 1:12) {
+    species_data <- species_data +
+      res_data[,i+offset]
+  }
+  species_data <- sqrt(species_data)
+  hist(main="R0", breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
+  lines(density(species_data), col="red")
+  CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
   abline(v=CIrand, col="blue", lwd=2)
-  dev.off()
-  pdf(paste("alpha.pdf"))
-  hist(main="alpha", breaks=100, freq=F, x=res_data[,2*offset-1], xlab="alpha", ylab="Likelihood")
-  lines(density(res_data[,2*offset-1]), col="red")
-  CIrand <- quantile(x=res_data[,14], probs=c(0.025, 0.975))
-  dev.off()
+  abline(v=1, col="green", lwd=2)
+  mtext(paste("p(R0>1) =", sum(species_data>1)/nrow(res_data)))
+
+  if (!is.null(opt$alpha)) {
+    ## pdf(paste("vprev.pdf"))
+    hist(main="vprev", breaks=100, freq=F, x=res_data[,offset], xlab="vprev", ylab="Likelihood")
+    lines(density(res_data[,offset]), col="red")
+    CIrand <- quantile(x=res_data[,offset], probs=c(0.025, 0.975))
+    abline(v=CIrand, col="blue", lwd=2)
+    ## dev.off()
+    ## pdf(paste("alpha.pdf"))
+    hist(main="alpha", breaks=100, freq=F, x=res_data[,2*offset-1], xlab="alpha", ylab="Likelihood")
+    lines(density(res_data[,2*offset-1]), col="red")
+    CIrand <- quantile(x=res_data[,14], probs=c(0.025, 0.975))
+    ## dev.off()
+  }
+  dev.off();
 }
