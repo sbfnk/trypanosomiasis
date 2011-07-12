bdaa469b5a2048abd24126ee87cfc7e41cef9038
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-07-12 16:24:42 +0000

    list animal contribution, too

--- human_contrib.R
+++ human_contrib.R
@@ -19,7 +19,16 @@ tab <- read.table(opt$file, header=T, sep=",")
 nam <- names(tab)
 res_data <- as.matrix(tab) 
 
-species_data<-res_data[,1+offset]
-h <- hist(main=nam[i+offset], breaks=100, freq=F, x=species_data, xlab="R0", ylab="Likelihood")
-CIrand <- quantile(x=species_data, probs=c(0.025, 0.975))
-cat(mean(species_data), ",", CIrand[1], ",", CIrand[2])
+human_data<-res_data[,1+offset]
+h <- hist(main=nam[1+offset], breaks=100, freq=F, x=human_data, xlab="R0", ylab="Likelihood")
+CIhuman <- quantile(x=human_data, probs=c(0.025, 0.975))
+
+animal_data <- rep(0, nrow(res_data))
+for (i in 2:12) {
+  animal_data <- animal_data +
+    res_data[,i+offset]^2
+}
+h <- hist(main=nam[1+offset], breaks=100, freq=F, x=animal_data, xlab="R0", ylab="Likelihood")
+CIanimal <- quantile(x=animal_data, probs=c(0.025, 0.975))
+
+cat(mean(human_data), ",", CIhuman[1], ",", CIhuman[2], ",", mean(animal_data), ",", CIanimal[1], ",", CIanimal[2], sep="")
