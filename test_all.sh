c6f3592a370b0dfaa6fdd44b70b52f17d373d9a6
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-08-17 15:03:34 +0000

    all in one file

--- test_all.sh
+++ test_all.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-bin/reservoirs -l 100 -n 100000 > single_none.csv
-bin/reservoirs -l 100 -n 100000 --groups "0;1,2,3;4,5,6,7,8,9,10,11" > hum_dom_wild_none.csv
-bin/reservoirs -l 100 -n 100000 --groups "0,1,2,3;4,5,6,7,8,9,10,11" > humdom_wild_none.csv
-bin/reservoirs -l 100 -n 100000 --groups "0;1,2,3,4,5,6,7,8,9,10,11" > hum_domwild_none.csv
-bin/reservoirs -l 100 -n 100000 -a b > single_binary.csv
-bin/reservoirs -l 100 -n 100000 -a b --groups "0;1,2,3;4,5,6,7,8,9,10,11" > hum_dom_wild_binary.csv
-bin/reservoirs -l 100 -n 100000 -a b --groups "0,1,2,3;4,5,6,7,8,9,10,11" > humdom_wild_binary.csv
-bin/reservoirs -l 100 -n 100000 -a b --groups "0;1,2,3,4,5,6,7,8,9,10,11" > hum_domwild_binary.csv
-bin/reservoirs -l 100 -n 100000 -a f > single_fractional.csv
-bin/reservoirs -l 100 -n 100000 -a f --groups "0;1,2,3;4,5,6,7,8,9,10,11" > hum_dom_wild_fractional.csv
-bin/reservoirs -l 100 -n 100000 -a f --groups "0,1,2,3;4,5,6,7,8,9,10,11" > humdom_wild_fractional.csv
-bin/reservoirs -l 100 -n 100000 -a f --groups "0;1,2,3,4,5,6,7,8,9,10,11" > hum_domwild_fractional.csv
+bin/reservoirs -l 100 -n 100000 -C groups,habitat -c single,none
+bin/reservoirs -l 100 -n 100000 --groups "0;1,2,3;4,5,6,7,8,9,10,11" -c hum_dom_wild,none
+bin/reservoirs -l 100 -n 100000 --groups "0,1,2,3;4,5,6,7,8,9,10,11" -c humdom_wild,none
+bin/reservoirs -l 100 -n 100000 --groups "0;1,2,3,4,5,6,7,8,9,10,11" -c hum_domwild,none
+bin/reservoirs -l 100 -n 100000 -a b -c single,binary
+bin/reservoirs -l 100 -n 100000 -a b --groups "0;1,2,3;4,5,6,7,8,9,10,11" -c hum_dom_wild,binary
+bin/reservoirs -l 100 -n 100000 -a b --groups "0,1,2,3;4,5,6,7,8,9,10,11" -c humdom_wild,binary
+bin/reservoirs -l 100 -n 100000 -a b --groups "0;1,2,3,4,5,6,7,8,9,10,11" -c hum_domwild,binary
+bin/reservoirs -l 100 -n 100000 -a f -c single,fractional
+bin/reservoirs -l 100 -n 100000 -a f --groups "0;1,2,3;4,5,6,7,8,9,10,11" -c hum_dom_wild,fractional
+bin/reservoirs -l 100 -n 100000 -a f --groups "0,1,2,3;4,5,6,7,8,9,10,11" -c humdom_wild,fractional
+bin/reservoirs -l 100 -n 100000 -a f --groups "0;1,2,3,4,5,6,7,8,9,10,11" -c hum_domwild,fractional
