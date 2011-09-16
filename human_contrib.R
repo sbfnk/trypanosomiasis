fb4cee99392b824369e7a11778b15c09b6c60649
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2011-09-16 16:18:55 +0000

    species data in other file

--- human_contrib.R
+++ human_contrib.R
@@ -5,12 +5,11 @@ confint <- function(x) {
   data.frame(y=mean, ymin=quant[1], ymax=quant[2])
 }
 
-species_data <- data.frame(db[hosts], db[domains[-1]], N=renv$Human_N,
-  xi=renv$G._palpalis_gambiense_xi, groups=renv$groups, habitat=renv$habitat)
-
+pdf('human_contrib.pdf')
 ggplot(subset(species_data, groups=="random"), aes(x=N/3540,y=Human))+
   stat_summary(fun.data="confint", geom="smooth", colour="red",
                alpha=0.4)+ theme_bw()+
   scale_x_continuous("Fraction of human population exposed")+
   scale_y_continuous(substitute(R[0]*" in system of humans and vector"))+
   geom_abline(intercept=1,slope=0) 
+dev.off()
