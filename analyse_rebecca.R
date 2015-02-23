6b9df8e376d61cbe5727261927f7a5aee38dd62a
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-02-23 10:50:31 +0000

    collate villages

--- analyse_rebecca.R
+++ analyse_rebecca.R
@@ -1,6 +1,34 @@
 library('XLConnect')
 library('data.table')
+library('ggplot')
+library('ggthemr')
 
-wb <- loadWorkbook("~/Research/Francesco/MSc\ project\ Rebecca\ Oettle/Results/Village10_Result1.xlsx", create = FALSE)
-df <- readWorksheet(wb, sheet = "Sheet3")
-dt <- data.table(df)
+path <- path.expand("~/Research/Francesco/MSc\ project\ Rebecca\ Oettle/Results/")
+
+files.list <- list.files(path, pattern = "Village.+.xlsx")
+
+village_data <- list()
+
+for (file in files.list)
+{
+    village_number <- sub("Village([0-9]+).+$", "\\1", file)
+    
+    wb <- loadWorkbook(paste0(path, "/", file), create = FALSE)
+    df <- readWorksheet(wb, sheet = "Sheet3")
+
+    village_data[[village_number]] <- copy(df)
+}
+
+
+plot_marginal <- function(df, var)
+{
+    ggthemr('fresh')
+    
+    dt <- data.table(df)
+    func_dt <- dt[, list(Fit = mean(Fit)), by = var]
+    p <- ggplot(func_dt, aes_string(x = var, y = "Fit"))
+    p <- p + geom_line()
+    return(p)
+}
+
+plot_marginal(dt, "Stage1")
