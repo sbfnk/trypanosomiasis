6a80a6bb4976e4b94dca9598cd3b13cec2572918
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-11-19 15:43:22 +0000

    analysis script rewrite

--- chronic_analysis.r
+++ chronic_analysis.r
@@ -1,38 +1,91 @@
+############################################################################
+## Script for analysing chronic carriers of trypanosomiasis               ##
+############################################################################
+
+library('docopt')
+
+"Script for analysing chronic carriers of trypanosomiasis
+Usage: chronic_analysis.r [options]
+Options:
+-v --village=<village>                       number of the village
+-h --help                                    show this message" -> doc
+
+opts <- docopt(doc)
+
+if (opts[["help"]])
+{
+    print(opts)
+    exit()
+}
+
+village_string <- opts[["nsamples"]]
+villages <- as.integer(unlist(strsplit(village_string, ",")))
+
 library('data.table')
 library('trypR')
 
-village <- 1
+village <- villages[1]
 
-system.time(lhs_samples <- chronic_carriers_lhs(passive = FALSE, nsamples = 1000, nruns = 5, verbose = FALSE, village.number = 1, calc.posterior = TRUE))
+lhs <- chronic_carriers_sample(nruns = 1, nsamples = 100, calc.posterior = FALSE,
+                                   villages = village, passive = TRUE, sample = "lhs")
+prior <- chronic_carriers_sample(nruns = 1, nsamples = 10000, calc.posterior = FALSE,
+                                 villages = village, passive = TRUE, sample = "prior")
 
-theta <- rprior(villages = 1, passive = FALSE)
+lhs_new <- chronic_carriers_sample(nruns = 1, nsamples = 10000, calc.posterior = FALSE,
+                                   villages = village, passive = TRUE, sample = "lhs",
+                                   transmitted = TRUE)
+prior_new <- chronic_carriers_sample(nruns = 1, nsamples = 10000,
+                                     calc.posterior = FALSE,
+                                     villages = village, passive = TRUE,
+                                     sample = "prior", transmitted = TRUE)
 
-current.posterior <- traj_likelihood(theta, 1, 5, log = TRUE, passive = FALSE)
-
-acceptance <- 0
-for (i in 1:10000)
+dt_lhs <- data.table(t(sapply(lhs, function(x)
 {
-    proposal.posterior <- traj_likelihood(theta, 1, 5, log = TRUE, passive = FALSE)
-    if (runif(1) < exp(proposal.posterior - current.posterior))
-    {
-        current.posterior <- proposal.posterior
-        acceptance <- acceptance + 1
-    }
-    cat(acceptance / i, "\n")
-}
-
-
-lhs_posteriors <- sapply(seq_along(lhs_samples), function(x)
+    c(x[["parameters"]], unlist(x[["summary_statistics"]]))
+})))
+dt_prior <- data.table(t(sapply(prior, function(x)
 {
-    lhs_samples[[x]][["posterior"]]
-})
-
-## param_posterior_villages(theta = samples[[92]]$parameters, nruns = 100, log = TRUE)
-
-top.10 <- order(lhs_posteriors, decreasing = TRUE)[1:10]
-
-top10_lhs_posteriors <- lhs_posteriors[top.10]
-top10_lhs_samples<- sapply(top.10, function(x)
+    c(x[["parameters"]], unlist(x[["summary_statistics"]]))
+})))
+dt_lhs_new <- data.table(t(sapply(lhs_new, function(x)
 {
-    lhs_samples[[x]][["parameters"]]
-})
+    c(x[["parameters"]], unlist(x[["summary_statistics"]]))
+})))
+dt_prior_new <- data.table(t(sapply(prior_new, function(x)
+{
+    c(x[["parameters"]], unlist(x[["summary_statistics"]]))
+})))
+
+dt_lhs[, sample := "lhs"]
+dt_prior[, sample := "prior"]
+
+dt <- rbind(dt_lhs, dt_prior)
+
+dt_lhs_new[, sample := "lhs"]
+dt_prior_new[, sample := "prior"]
+dt_new <- rbind(dt_lhs_new, dt_prior_new)
+
+data_stage1_active <- village_screening[village.number == village, detected1_1]
+data_stage1_passive <-
+    village_cases[village.number == village & stage == 1, sum(cases)]
+data_stage2_passive <-
+    village_cases[village.number == village & stage == 2, sum(cases)]
+
+dt[, active_stage1_diff := active_stage1 - data_stage1_active]
+dt[, passive_stage1_diff := passive_stage1 - data_stage1_passive]
+dt[, passive_stage2_diff := passive_stage2 - data_stage2_passive]
+dt_new[, active_stage1_diff := active_stage1 - data_stage1_active]
+dt_new[, passive_stage1_diff := passive_stage1 - data_stage1_passive]
+dt_new[, passive_stage2_diff := passive_stage2 - data_stage2_passive]
+
+threshold <- 0
+
+dt[, accept := (abs(active_stage1_diff) <= threshold) & (abs(passive_stage1_diff) <= threshold) & (abs(passive_stage2_diff) <= threshold)]
+dt_new[, accept := (abs(active_stage1_diff) <= threshold) & (abs(passive_stage1_diff) <= threshold) & (abs(passive_stage2_diff) <= threshold)]
+
+good <- dt[accept == TRUE]
+good_new <- dt_new[accept == TRUE]
+
+prior_samples <- dt[sample == "prior"]
+prior_samples[, sample := NULL]
+saveRDS(prior_samples, "prior_samples.rds")
