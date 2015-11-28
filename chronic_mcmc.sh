aa7d75c30b17102ddcfee184f833e6ded8ed41ec
sbfnk <sbfnk@users.noreply.github.com>
2015-11-28 12:39:30 +0000

    no library path needed with new cluster

--- chronic_mcmc.sh
+++ chronic_mcmc.sh
@@ -3,5 +3,5 @@
 #$ -M sebastian.funk@lshtm.ac.uk
 #$ -m n
 #$ -l mem_free=300M,h_vmem=6000M
-#$ -t 1:26
-LD_LIBRARY_PATH=/opt/intel/compiler101/x86_64/lib:$LD_LIBRARY_PATH Rscript $HOME/code/trypanosomiasis/chronic_mcmc.R ${SGE_TASK_ID}
+#$ -t 1-26
+Rscript $HOME/code/trypanosomiasis/chronic_run.r -v ${SGE_TASK_ID} -n 1000000
