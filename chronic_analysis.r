d5691dc1bf085b6ea294c55e27c1034614341a6e
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-02-04 09:27:30 +0000

    different models

--- chronic_analysis.r
+++ chronic_analysis.r
@@ -1,4 +1,5 @@
 library('dplyr')
+library('tidyr')
 library('coda')
 library('modeest')
 library('cowplot')
@@ -33,17 +34,23 @@ library('bde')
 ## runs <- data.table(m)
 ## setnames(runs, 1:2, c("run", "epsilon"))
 
-limits <- list(pc = c(0, 0.5), alpha = c(0, 1), delta = c(0, 1), p1 = c(0, 30), p2 = c(0, 30), beta = c(10^(-5), 10^(0)), lambda = c(10^(-5), 10^(0)))
+limits <- list(pc = c(0, 0.5), alpha = c(0, 1), delta = c(0, 1), p1 = c(0, 30), p2 = c(0, 30), beta = c(0, 10^(0)), lambda = c(0, 10^(0)))
 
-## for (model in c("tran_back_chro", "back"))
-for (model in c("tran_back_chro"))
+mcmc <- list()
+village_density <- list()
+densities <- list()
+dt <- list()
+traces <- list()
+
+## for (model in c("tran_back_chro", "tran_back", "back"))
+for (model in c("tran_back_chro", "tran_back"))
 {
     file_path <- path.expand("~/Data/Trypanosomiasis/")
     file_pattern <- paste(model, ".*", sep = "_")
     file_nos <- as.integer(gsub("[^0-9]", "",
                                 list.files(path = file_path, pattern = file_pattern)))
 
-    mcmc <- list()
+    mcmc[[model]] <- list()
     i <- 0
     ## file_nos <- runs[epsilon <= 1, run]
 
@@ -52,115 +59,111 @@ for (model in c("tran_back_chro"))
         i <- i + 1
         file_name <-
             path.expand(paste0(file_path, "/", model, "_village_", file_no, ".rds"))
-        mcmc[[i]] <- readRDS(file_name)
-        mcmc[[i]][["trace"]] <- mcmc[[i]][["trace"]] %>% mutate(village = file_no)
+        mcmc[[model]][[i]] <- readRDS(file_name)
+        mcmc[[model]][[i]][["trace"]] <- mcmc[[model]][[i]][["trace"]] %>%
+            mutate(village = file_no)
     }
 
-    traces <- rbind_all(lapply(mcmc, function(x) {x[["trace"]]}))
-    acceptance.rates <- data.frame(file_no = file_nos, acceptance.rate = sapply(mcmc, function(x) {x[["acceptance.rate"]]}))
-    traces <- traces %>% mutate(id = rep(seq(0, nrow(traces) / length(file_nos) - 1), length(file_nos)))
+    traces[[model]] <- rbind_all(lapply(mcmc[[model]], function(x) {x[["trace"]]})) %>%
+        mutate(model = model)
+    acceptance.rates <- data.frame(file_no = file_nos, acceptance.rate = sapply(mcmc[[model]], function(x) {x[["acceptance.rate"]]}))
+    traces[[model]] <- traces[[model]] %>% mutate(id = rep(seq(0, n() / length(file_nos) - 1), length(file_nos)))
 
     ## burn-in
-    traces <- traces %>% filter(id > 99)
-    ## ggplot(thin.traces, aes(x = pc))+geom_density()+facet_wrap(~village)
+    traces[[model]] <- traces[[model]] %>% filter(id > 99)
+    ## ggplot(thin.traces[[model]], aes(x = pc))+geom_density()+facet_wrap(~village)
 
     ## Kernel density estimation
 
-    densities <- list()
-    hists <- list()
+    densities[[model]] <- list()
+    village_density[[model]] <- list()
 
-    village_density <- list()
-    village_hist <- list()
-
-    dt <- list()
-    dtm <- list()
+    dt[[model]] <- list()
+    dtm[[model]] <- list()
 
     for (param in c("pc", "alpha", "delta", "p1", "p2", "beta", "lambda"))
     {
-
-        densities[[param]] <- list()
-        hists[[param]] <- list()
-        
-        min <- traces %>% select(get(param)) %>% min %>% floor
-        max <- traces %>% select(get(param)) %>% max %>% ceiling
-        
-        i <- 0
-        for (village.number in file_nos)
+        if (traces[[model]] %>% select(get(param)) %>% unique %>% nrow > 1)
         {
-            village_param <- traces %>% filter(village == village.number) %>% .[[param]]
-            i <- i + 1
-            densities[[param]][[i]] <- bde(village_param, 
-                                           estimator = "vitale", lower.limit = limits[[param]][1],
-                                           upper.limit = limits[[param]][2])
-            hists[[param]][[i]] <- hist(village_param, 
-                                        breaks = seq(limits[[param]][1], limits[[param]][2],
-                                                     length.out = 101),
-                                        plot = FALSE)
+
+            densities[[model]][[param]] <- list()
+
+            min <- traces[[model]] %>% select(get(param)) %>% min %>% floor
+            max <- traces[[model]] %>% select(get(param)) %>% max %>% ceiling
+
+            i <- 0
+            for (village.number in file_nos)
+            {
+                village_param <- traces[[model]] %>%
+                    filter(village == village.number) %>% .[[param]]
+                i <- i + 1
+                densities[[model]][[param]][[i]] <-
+                    bde(village_param,
+                        estimator = "vitale", lower.limit = limits[[param]][1],
+                        upper.limit = limits[[param]][2])
+            }
+
+            densm_x <- sapply(densities[[model]][[param]],
+                              function(x) {x@lower.limit +
+                                               x@dataPointsCache * x@upper.limit})
+            densm_y <- sapply(densities[[model]][[param]], function(x) {x@densityCache})
+
+            dt[[model]][[param]] <-
+                data.frame(x = densm_x[, 1], y = apply(densm_y, 1, prod))
+            ## normalise
+            dt[[model]][[param]] <- dt[[model]][[param]] %>%
+                mutate(y.norm = y * (length(x) - 1) / (sum(y) * (max(x) - min(x))))
+
+            village_density[[model]][[param]] <-
+                densm_y %>% as.data.frame %>% gather %>%
+                mutate(file = as.integer(key), param = param,
+                       param.value = rep(densm_x[, 1], dim(densm_x)[2]),
+                       density = value, model = model) %>%
+                mutate(village = file_nos[file]) %>%
+                select(model, param, village, param.value, density)
         }
 
-        densm_x <- sapply(densities[[param]], function(x) {x@lower.limit + x@dataPointsCache * x@upper.limit})
-        densm_y <- sapply(densities[[param]], function(x) {x@densityCache})
-        histsm_x <- sapply(hists[[param]], function(x) log(x[["mids"]]))
-        histsm_y <- sapply(hists[[param]], function(x) log(x[["density"]]))
-
-        dt[[param]] <- data.frame(x = densm_x[, 1], y = apply(densm_y, 1, prod))
-        dtm[[param]] <- data.frame(x = histsm_x[, 1],y = exp(apply(histsm_y, 1, sum)))
-
-        ## normalise
-        dt[[param]] <- dt[[param]] %>%
-            mutate(y.norm = y * (length(x) - 1) / (sum(y) * (max(x) - min(x))))
-        dtm[[param]] <- dtm[[param]] %>%
-            mutate(y.norm = y * (length(x) - 1) / (sum(y) * (max(x) - min(x))))
-
-        village_density[[param]] <-
-            densm_y %>% as.data.frame %>% gather %>%
-            mutate(village = as.integer(key), param = param, param.value = rep(densm_x[, 1], dim(densm_x)[2]), density = value) %>%
-            select(param, village, param.value, density)
-
-        village_hist[[param]] <-
-            histsm_y %>% as.data.frame %>% gather %>%
-            mutate(village = as.integer(key), param = param, param.value = rep(histsm_x[, 1], dim(densm_x)[2]), density = value) %>%
-            select(param, village, param.value, density)
     }
+    village_density[[model]] <- rbind_all(village_density[[model]])
 }
 
 village_density <- rbind_all(village_density)
-village_hist <- rbind_all(village_hist)
 
-for (param in c("pc", "alpha", "delta", "p1", "p2", "beta", "lambda"))
-{
-    p <- ggplot(dt[[param]], aes(x = x, y = y.norm))+geom_line()
-    ggsave(paste0("density_", param, ".pdf"))
-##    p <- ggplot(dtm[[param]], aes(x = x, y = y.norm))+geom_bar(stat = "identity")
-##    ggsave(paste0("histogram_", param, ".pdf"))
-}
-
-
-## check caterpillars
 p <- list()
-
-for (this.param in c("pc", "alpha", "delta", "p1", "p2", "lambda", "beta"))
+for (model in c("tran_back_chro", "tran_back", "back"))
 {
-    p[[this.param]] <- list()
-    p[[this.param]][["trace"]] <- ggplot(traces, aes_string(x = "id", y = this.param))
-    p[[this.param]][["trace"]] <- p[[this.param]][["trace"]] + geom_line()
-    p[[this.param]][["trace"]] <- p[[this.param]][["trace"]] + facet_wrap(~village,
-                                                                scales = "free")
-    ggsave(paste0("caterpillars_", this.param, ".pdf"), p[[this.param]][["trace"]],
-           width = 14, height = 14)
+    for (param in names(dt[[model]]))
+    {
+        p <- ggplot(dt[[model]][[param]], aes(x = x, y = y.norm))+geom_line()
+        ggsave(paste0("density_", model, "_", param, ".pdf"))
+    }
 
-    p[[this.param]][["density"]] <- ggplot(village_density %>% filter(param == this.param),
-                                           aes(x = param.value, y = density))
-    p[[this.param]][["density"]] <- p[[this.param]][["density"]] + geom_line()
-    p[[this.param]][["density"]] <- p[[this.param]][["density"]] + facet_wrap(~village,
-                                                                    scales = "free")
-    ggsave(paste0("densities_", this.param, ".pdf"), p[[this.param]][["density"]],
-           width = 14, height = 14)
-    p[[this.param]][["histogram"]] <- ggplot(village_hist %>% filter(param == this.param),
-                                             aes(x = param.value, y = density))
-    p[[this.param]][["histogram"]] <- p[[this.param]][["histogram"]] + geom_line()
-    p[[this.param]][["histogram"]] <- p[[this.param]][["histogram"]] + facet_wrap(~village,
-                                                                        scales = "free")
-    ggsave(paste0("histograms_", this.param, ".pdf"), p[[this.param]][["histogram"]],
-           width = 14, height = 14)
+
+    ## check caterpillars
+    p[[model]] <- list()
+
+    for (this.param in names(dt[[model]]))
+    {
+        p[[this.param]] <- list()
+        p[[model]][[this.param]][["trace"]] <-
+            ggplot(traces[[model]], aes_string(x = "id", y = this.param))
+        p[[model]][[this.param]][["trace"]] <-
+            p[[model]][[this.param]][["trace"]] + geom_line()
+        p[[model]][[this.param]][["trace"]] <-
+            p[[model]][[this.param]][["trace"]] + facet_wrap(~village, scales = "free")
+        ggsave(paste0("caterpillars_", model, "_", this.param, ".pdf"),
+               p[[model]][[this.param]][["trace"]], width = 14, height = 14)
+
+        p[[model]][[this.param]][["density"]] <-
+            ggplot(village_density %>% filter(param == this.param),
+                   aes(x = param.value, y = density))
+        p[[model]][[this.param]][["density"]] <-
+            p[[model]][[this.param]][["density"]] + geom_line()
+        p[[model]][[this.param]][["density"]] <-
+            p[[model]][[this.param]][["density"]] +
+            facet_wrap(~village, scales = "free")
+        ggsave(paste0("densities_", model, "_", this.param, ".pdf"),
+               p[[model]][[this.param]][["density"]],
+               width = 14, height = 14)
+    }
 }
