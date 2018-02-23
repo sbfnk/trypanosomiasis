d20decc6e5c276d49efcd970a11f874b9d6b3b65
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-02-23 08:20:55 +0000

    updated fig. based on PLoS requirements

--- passive/plots.R
+++ passive/plots.R
@@ -51,5 +51,8 @@ p <- ggplot(lambda, aes(x=id)) +
     expression(atop(Incidence~(lambda[i])~"in cases",
                     per~"1,000"~susceptible~"person-months"))) +
   theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
-ggsave("Passive/foi_villages.pdf", p, width=8.2, height=3.8)
+        axis.ticks.x=element_blank(),
+        strip.background =element_rect(fill="white"),
+        strip.text.x = element_text(size = 10))
+
+save_plot("Passive/foi_villages.pdf", p, base_aspect_ratio = 2, base_height=3.5)
